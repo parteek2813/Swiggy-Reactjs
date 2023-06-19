bbbff787c745ce9df06ebdc22013a916e9940ba3
parteek kumar <digitalparteek@gmail.com>
2023-06-19 17:46:38 +0530

    Added home shimer effect

--- src/Componenets/Shimer.js
+++ src/Componenets/Shimer.js
@@ -1,10 +1,13 @@
 const Shimer = () => {
     return (
-    <div className="cardlist">
-        {Array(10).fill("").map((e, index) =>
-         <div key={index} className="shimer-card"></div>)}
-    </div>
+        <div className="cardlist">
+            {Array(18)
+                .fill('')
+                .map((e, index) => (
+                    <div key={index} className="shimer-card"></div>
+                ))}
+        </div>
     );
-}
+};
 
 export default Shimer;
